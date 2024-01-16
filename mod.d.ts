c0aa0e806387d6ed74fd7aacdce8cb6854174755
Phil Chen <06fahchen@gmail.com>
2024-01-16 15:12:20 +0800

    fix: use declare to export as global namespace

--- mod.d.ts
+++ mod.d.ts
@@ -1,25 +1,72 @@
-export namespace BreezeRuntime {
+declare namespace BreezeRuntime {
   export interface Env {
+    /**
+     * Retrieves the value of an environment variable.
+     * @param key The name of the environment variable.
+     * @returns The value of the environment variable, or undefined if it is not set.
+     *
+     * @example
+     * ```ts
+     * BreezeRuntime.env.get("foo");
+     * // Returns "bar" if the environment variable "foo" is set to "bar".
+     * ```
+     */
     get(key: string): string | undefined;
+
+    /**
+     * Retrieves all environment variables.
+     * @returns A map of all environment variables.
+     *
+     * @example
+     * ```ts
+     * BreezeRuntime.env.toObject();
+     * // Returns { foo: "bar" } if the environment variable "foo" is set to "bar".
+     * ```
+     */
     toObject(): Record<string, string | undefined>;
+
+    /**
+     * Checks whether an environment variable is set.
+     * @param key The name of the environment variable.
+     * @returns Whether the environment variable is set.
+     *
+     * @example
+     * ```ts
+     * BreezeRuntime.env.has("foo");
+     * // Returns true if the environment variable "foo" is set.
+     * ```
+     */
     has(key: string): boolean;
   }
 
   export interface Plugin {
     /**
-     * BreezeRuntime.plugins
+     * Retrieves the plugin's endpoint.
      *
      * @example
-     * Get endpoint of the kv plugin:
+     *
+     * To retrieve the endpoint of the kv plugin:
      * ```ts
      * BreezeRuntime.plugins.kv.getEndpoint();
-     * // return new URL("breeze://kv.plugins")
+     * // Returns new URL("breeze://kv.plugins")
+     * ```
+     *
+     * To retrieve the endpoint of the kv plugin under a subpath:
+     * ```ts
+     * BreezeRuntime.plugins.kv.getEndpoint("/put");
+     * // Returns new URL("breeze://kv.plugins/put")
      * ```
      */
     getEndpoint(subpath?: string): Promise<URL>;
   }
 
+  /**
+   * The environment variables.
+   */
   export const env: Env;
 
+  /**
+   * A map of all plugins.
+   */
   export const plugins: Record<string, Plugin>;
 }
