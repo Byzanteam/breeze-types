9d52fc40ecdb3abc5302a25cf9f95d78a938fa90
Phil Chen <06fahchen@gmail.com>
2025-02-19 19:41:31 +0800

    feat: log server url

--- lib/runtime.ts
+++ lib/runtime.ts
@@ -16,7 +16,16 @@ export function getEnvOrThrow(key: string): string {
 export function serve(
   handler: (req: Request) => Response | Promise<Response>,
 ) {
-  Deno.serve({ port: 0 }, handler);
+  Deno.serve(
+    {
+      port: 0,
+      onListen() {
+        const url = new URL(getEnvOrThrow("JET_BREEZE_PATH_PREFIX"), getEnvOrThrow("JET_BREEZE_HOST"));
+        console.log(`Listening on ${url.toString()}`);
+      },
+    },
+    handler,
+  );
 }
 
 function doGetEnv(key: string): string | undefined {
